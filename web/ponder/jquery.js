4cf8b9ce76345d4f3ee06bd5f48cea9b9930fe0f
Steven <and12128@byui.edu>
2018-09-26 16:10:02 -0700

    modified ponder files

--- web/ponder/jquery.js
+++ web/ponder/jquery.js
@@ -6,8 +6,8 @@ function changeColor() {
     var textbox_id = "txtColor";
     var textbox = document.getElementById(textbox_id);
 
-    var div_id = "div1";
-    var div = document.getElementById(div_id);
+    var div_class = "one";
+    var div = document.getElementByClassName(div_class);
 
     // We should verify values here before we use them...
     var color = textbox.value;
