3fefef4880dd2c63b90f8f289558c4a6a612bf52
Steven <and12128@byui.edu>
2018-09-29 10:40:29 -0700

    homepage.php added

---
+++ web/footer.php
@@ -0,0 +1,18 @@
+<table class="footer" style="background-color: grey">
+          <tr>
+              <th> Other Work </th>
+              <th> Contact Info </th>
+              <th> Newsletter </th>
+          </tr>
+          <tr>
+              <td>
+                  <a href="prove.html"> Archive</a>
+              </td>
+              <td>
+                  1 (800) 555-1234 <br />
+                  probablywon'twork@something.com<br />
+              </td>
+              <td> SignUp </a></td>
+          </tr>
+      </table>
+      <div class="footerBar"></div>
---
+++ web/header.php
@@ -0,0 +1,30 @@
+<div class="headerBar"></div>
+
+
+      <h1 id = "top"
+          class = "home"><a href = "Homepage.html"> <em>
+            Home Page</em></a></h1>
+      <br /> <br />
+         
+    <a href="mailto:and12128@byui.edu" class="email"> AND12128@BYUI.EDU</a>
+     
+      <hr class = "top"/>
+      
+      <table class = "archive">
+        <tr>
+          <td>
+            <a href = "Homepage.html"
+               class = "tile"> HOME </a></td>
+          <td>
+            <a href = "prove.html"
+               class = "tile" > PROVE </a></td>
+          <td>
+            <a href = ""
+               class = "tile"> PONDER </a></td>
+          <td>
+            <a href = ""
+               class = "tile"> CLASS </a></td>
+          <td>
+            <a href = "mailto:and1212*@byui.edu"
+               class = "tile" > CONTACT </a></td></tr></table>
+       
