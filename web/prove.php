90fe9360135727c8adc23ce3d406c58b62972a3a
Steven <and12128@byui.edu>
2018-09-29 17:17:40 -0700

    adding Prove.php

---
+++ web/prove.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC >
+<html lang = "english">
+  
+  <head><title> Prove Assignements </title>
+    <meta charset = "utf-8">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
+  <body>   
+<?php
+ include('header.php')
+?>
+<h1 class ="assignList"> List of Assignements </h1>
+
+<ul class ="assignList">
+<li><a href = "index.html"> index.html </a></li>
+        <li><a href = "Homepage.php"> Week 2 - Home Page </a></li>
+        <li><a href = ""> Week 3 </a></li>
+        <li><a href = ""> Week 4 </a></li>
+        <li><a href = ""> Week 5 </a></li>
+        <li><a href = ""> Week 6 </a></li>
+        <li><a href = ""> Week 7 </a></li>
+        <li><a href = ""> Week 8 </a></li>
+        <li><a href = ""> Week 9 </a></li>
+        <li><a href = ""> Week 10 </a></li>
+        <li><a href = ""> Week 11 </a></li>
+        <li><a href = ""> Week 12 </a></li>
+        <li><a href = ""> Week 13 </a></li>
+<?php
+include('footer.php')
+?>
