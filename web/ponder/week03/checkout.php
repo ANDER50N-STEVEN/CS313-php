dfc7db1fff14ece6592e27a260942e6c56a50c42
Steven <and12128@byui.edu>
2018-10-06 15:42:22 -0700

    week03

---
+++ web/ponder/week03/checkout.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC >
+<html lang = "english">
+  
+  <head><title> Home Page </title>
+    <meta charset = "utf-8">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
+  <body>      
+      <?php
+      include('header.php')
+      ?>
+	  <div class ="wrapper">
+      <table class="about">
+          <tr>
+              <th class="aboutMe"> Interests</th>
+              <th bgcolor="#595959"
+                  class="about"
+                  style="font-family: sans-serif">Life</th>
+          </tr>
+          <td class="aboutMe">
+              Hi, my name is Steven Anderson.  I feel like you never realize how
+			boring your life is until you try and describe your interests.  Anyway, 
+			some of the things I like doing are hiking/camping even though I feel like
+			I might be getting too old for that, or at least my body thinks so. <br />
+              Along with the outdoors, I love reading.  My dream one day is to have a
+			  sitting room in my home where I can have thousands of books on shelves and
+			  have the time to read anything and everything I want.
+          </td>
+          <td style bgcolor="#4d4d4d"
+              class="about">
+              My wife and I recently moved to Portland oregon because she just graduated
+				and got a job out here. Our hope is that I will be able to finish my degree
+				 online.  Originally I am from Southern California so the rain is something
+I'm going to have to get used to, but it'll be better than the sub zero tempuratures in Rexburg. 
+           
+          </td>
+      </table>
+
+	  <img class = "homeImg" src = "National Harbor.png" alt = "National Harbor with Kate Lynn">
+	  </div>
+      <?php
+	  include('footer.php')
+	  ?>
+  </body>
+</html>
---
+++ web/ponder/week03/header.php
@@ -0,0 +1,29 @@
+<div class="headerBar"></div>
+
+      <h1 id = "top"
+          class = "home"><a href = "Homepage.php"> <em>
+            Home Page</em></a>
+
+    <a href="mailto:and12128@byui.edu" class="email"> 
+		<img src="email.jpg" alt="EMAIL" style="width:40px;height:30px;border:0;">
+		</a>   </h1>
+      <hr class = "top"/>
+      
+      <table class = "archive">
+        <tr>
+          <td class = "archive">
+            <a href = "Homepage.php"
+               class = "tile"> HOME </a></td>
+          <td class = "archive">
+            <a href = "prove.php"
+               class = "tile" > PROVE </a></td>
+          <td class = "archive">
+            <a href = ""
+               class = "tile"> PONDER </a></td>
+          <td class = "archive">
+            <a href = "https://byui.brightspace.com/d2l/le/content/490016/viewContent/6811648/View"
+               class = "tile"> CLASS </a></td>
+          <td class = "archive">
+            <a href = "mailto:and1212*@byui.edu"
+               class = "tile" > CONTACT </a></td></tr></table>
+       
