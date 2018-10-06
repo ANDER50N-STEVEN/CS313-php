3441786a720f42c9899af2c25611355965df3194
Steven <and12128@byui.edu>
2018-10-06 16:09:49 -0700

    organized folders

--- web/prove.php
+++ web/prove.php
@@ -13,17 +13,17 @@
 
 <ul class ="assignList">
         <li><a href = "Homepage.php"> Week 2 - Home Page </a></li>
-        <li><a href = ""> Week 3 </a></li>
-        <li><a href = ""> Week 4 </a></li>
-        <li><a href = ""> Week 5 </a></li>
-        <li><a href = ""> Week 6 </a></li>
-        <li><a href = ""> Week 7 </a></li>
-        <li><a href = ""> Week 8 </a></li>
-        <li><a href = ""> Week 9 </a></li>
-        <li><a href = ""> Week 10 </a></li>
-        <li><a href = ""> Week 11 </a></li>
-        <li><a href = ""> Week 12 </a></li>
-        <li><a href = ""> Week 13 </a></li>
+        <li><a href = "prove/week03/browse.php"> Week 3 </a></li>
+        <li><a href = "prove/week04/browse.php"> Week 4 </a></li>
+        <li><a href = "prove/week05/browse.php"> Week 5 </a></li>
+        <li><a href = "prove/week06/browse.php"> Week 6 </a></li>
+        <li><a href = "prove/week07/browse.php"> Week 7 </a></li>
+        <li><a href = "prove/week08/browse.php"> Week 8 </a></li>
+        <li><a href = "prove/week09/browse.php"> Week 9 </a></li>
+        <li><a href = "prove/week10/browse.php"> Week 10 </a></li>
+        <li><a href = "prove/week11/browse.php"> Week 11 </a></li>
+        <li><a href = "prove/week12/browse.php"> Week 12 </a></li>
+        <li><a href = "prove/week13/browse.php"> Week 13 </a></li>
 		</ul>
 <?php
 include('footer.php')
