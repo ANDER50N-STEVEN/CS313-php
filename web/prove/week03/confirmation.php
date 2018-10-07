4aa6330d0b4ea427fada8092d6b6866f3f8d1761
Steven <and12128@byui.edu>
2018-10-06 18:30:22 -0700

    confirmation session

--- web/prove/week03/confirmation.php
+++ web/prove/week03/confirmation.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+$products = $_POST["checkbox"];
+
+if (empty($_SESSION["cart"])){
+    $_SESSION["cart"] = array[];
+}
+
+array_push($_SESSION["cart"], $products)
+?>
+
+
 <!DOCTYPE html PUBLIC >
 <html lang = "english">
   
@@ -9,35 +22,15 @@
       <?php
       include('header.php')
       ?>
-	  <div class ="wrapper">
-      <table class="about">
-          <tr>
-              <th class="aboutMe"> Interests</th>
-              <th bgcolor="#595959"
-                  class="about"
-                  style="font-family: sans-serif">Life</th>
-          </tr>
-          <td class="aboutMe">
-              Hi, my name is Steven Anderson.  I feel like you never realize how
-			boring your life is until you try and describe your interests.  Anyway, 
-			some of the things I like doing are hiking/camping even though I feel like
-			I might be getting too old for that, or at least my body thinks so. <br />
-              Along with the outdoors, I love reading.  My dream one day is to have a
-			  sitting room in my home where I can have thousands of books on shelves and
-			  have the time to read anything and everything I want.
-          </td>
-          <td style bgcolor="#4d4d4d"
-              class="about">
-              My wife and I recently moved to Portland oregon because she just graduated
-				and got a job out here. Our hope is that I will be able to finish my degree
-				 online.  Originally I am from Southern California so the rain is something
-I'm going to have to get used to, but it'll be better than the sub zero tempuratures in Rexburg. 
-           
-          </td>
-      </table>
 
-	  <img class = "homeImg" src = "National Harbor.png" alt = "National Harbor with Kate Lynn">
-	  </div>
+
+
+      <br />
+<br />
+<br />
+<br />
+<br />
+
       <?php
 	  include('footer.php')
 	  ?>
