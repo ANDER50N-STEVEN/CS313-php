e326db307349fe1c97c6270b22155fe4a7d6eec8
Steven <and12128@byui.edu>
2018-10-06 23:36:15 -0700

    trying to get other pages working

--- web/prove/week03/checkout.php
+++ web/prove/week03/checkout.php
@@ -1,45 +1,77 @@
-<!DOCTYPE html PUBLIC >
-<html lang = "english">
-  
-  <head><title> Home Page </title>
-    <meta charset = "utf-8">
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">
-  </head>
-  <body>      
-      <?php
-      include('header.php')
-      ?>
-	  <div class ="wrapper">
-      <table class="about">
-          <tr>
-              <th class="aboutMe"> Interests</th>
-              <th bgcolor="#595959"
-                  class="about"
-                  style="font-family: sans-serif">Life</th>
-          </tr>
-          <td class="aboutMe">
-              Hi, my name is Steven Anderson.  I feel like you never realize how
-			boring your life is until you try and describe your interests.  Anyway, 
-			some of the things I like doing are hiking/camping even though I feel like
-			I might be getting too old for that, or at least my body thinks so. <br />
-              Along with the outdoors, I love reading.  My dream one day is to have a
-			  sitting room in my home where I can have thousands of books on shelves and
-			  have the time to read anything and everything I want.
-          </td>
-          <td style bgcolor="#4d4d4d"
-              class="about">
-              My wife and I recently moved to Portland oregon because she just graduated
-				and got a job out here. Our hope is that I will be able to finish my degree
-				 online.  Originally I am from Southern California so the rain is something
-I'm going to have to get used to, but it'll be better than the sub zero tempuratures in Rexburg. 
-           
-          </td>
-      </table>
+<?php
+session_start();
 
-	  <img class = "homeImg" src = "National Harbor.png" alt = "National Harbor with Kate Lynn">
-	  </div>
-      <?php
-	  include('footer.php')
-	  ?>
-  </body>
+$products = $_POST['checkbox'];
+$total = 0;
+
+if (empty($_SESSION['cart'])){
+    $_SESSION['cart'] = array();
+    $_SESSION['book'] = array();
+    $_SESSION['cost'] = array();
+    $_SESSION['img'] = array();
+}
+
+foreach($products as $product){
+    array_push($_SESSION['cart'], $product);
+}
+
+?>
+
+
+<!DOCTYPE html PUBLIC >
+<html lang="english">
+
+<head>
+    <title> Home Page </title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+</head>
+<body>
+    <?php
+    include('header.php')
+    ?>
+
+    <br />
+    <?php
+    var_dump($_SESSION['cart']);
+    ?>
+
+    <table>
+        <tr>
+            <th>Image</th>
+            <th>Item</th>
+            <th>Cost</th>
+        </tr>
+        <?php
+        foreach($_SESSION['cart'] as $item){
+            $temp = explode("|", $item);
+            echo "<tr>
+                        <td >$temp[2]</td>
+                        <td>$temp[0]</td>
+                        <td>$temp[1]</td>
+                        </tr>";
+            $total += $temp[1];
+        }
+        ?>
+    </table>
+    <br />
+
+    <br />
+    <form action="browse.php">
+        <input type="submit" value="Continue Shopping" />
+    </form>
+
+    <br />
+    <form action="confirmation.php">
+        <input type="submit" value="Checkout" />
+    </form>
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <?php
+    include('footer.php')
+    ?>
+</body>
 </html>
