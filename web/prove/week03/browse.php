715527753eab5323789946964da513630a86e733
Steven <and12128@byui.edu>
2018-10-06 18:14:25 -0700

    2d array

--- web/prove/week03/browse.php
+++ web/prove/week03/browse.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+
+
 ?>
 
 <!DOCTYPE html PUBLIC >
@@ -14,7 +16,7 @@ session_start();
       include('header.php')
       ?>
 	  
-<form action="cart.php" method="POST">
+<form action="confirmation.php" method="POST">
      <h3 id = "fantasy" class = "fantasy"> FANTASY </h3>
       <table>
         <tr>
@@ -22,56 +24,57 @@ session_start();
             <img src = "http://ecx.images-amazon.com/images/I/51Nq%2Bl67AEL._AA160_.jpg" ><br />
             <input id="LoTR" 
                    type="checkbox" name="checkbox[]" 
-                   value="The Lord of the Rings">
+                   value=array("The Lord of the Rings", 11.87, "http://ecx.images-amazon.com/images/I/51Nq%2Bl67AEL._AA160_.jpg")>
             <b>The Lord of the Rings</b>
             <br /> Price: $11.87</th>
           <th class="fantasy">
             <img src = "http://ecx.images-amazon.com/images/I/5164%2Bq5eGfL._AA160_.jpg " ><br />
             <input id="EoTW" 
                    type="checkbox" name="checkbox[]" 
-                   value="The Eye of the World" >
+                   value=array("The Eye of the World", 7.99, "http://ecx.images-amazon.com/images/I/5164%2Bq5eGfL._AA160_.jpg ") >
             <b>The Eye of the World</b>
             <br /> Price:$7.99 </th>
         <th class="fantasy">
           <img src = "http://ecx.images-amazon.com/images/I/51bjoG8C%2B4L._AA160_.jpg " ><br /> 
           <input id="WoK" 
                  type="checkbox" name="checkbox[]" 
-                 value="8.99" >
+                 value=array("The Way of Kings", 8.99, "http://ecx.images-amazon.com/images/I/51bjoG8C%2B4L._AA160_.jpg " ) >
           <b>The Way of Kings</b>
           <br /> Price: $8.99 </th>
         <th class="fantasy">
           <img src = "http://ecx.images-amazon.com/images/I/51jIQA2jq9L._AA160_.jpg " ><br />
           <input id="CT" 
                  type="checkbox" name="checkbox[]" 
-                 value="8.89" >
+                 value="The Crown Tower", 8.89, "http://ecx.images-amazon.com/images/I/51jIQA2jq9L._AA160_.jpg " >
           <b>The Crown Tower</b>
           <br /> Price: $8.89 </th><tr>
         <th class="fantasy">
           <img src = "http://ecx.images-amazon.com/images/I/51BmlSQXY5L._AA160_.jpg " ><br />
-          <input id="BtS" 
-                 type="checkbox" name="checkbox[]"
-                 value="7.99" >
+            <input id="BtS"
+                type="checkbox" name="checkbox[]"
+                value="Belgarath the Sorcerer", 7.99, "http://ecx.images-amazon.com/images/I/51BmlSQXY5L._AA160_.jpg " >
           <b>Belgarath the Sorcerer</b>
           <br /> Price: $7.99</th></a>
-        <th class="fantasy">
+        <th class="
+                fantasy" />
           <img src = "http://ecx.images-amazon.com/images/I/51HGCx5Rh6L._AA160_.jpg " ><br /> 
           <input id="NotW" 
                  type="checkbox" name="checkbox[]" 
-                 value="7.99" >
+                 value="The Name of the Wind", 7.99, "http://ecx.images-amazon.com/images/I/51HGCx5Rh6L._AA160_.jpg " >
           <b>The Name of the Wind</b>
           <br /> Price: $7.99</th>
         <th class="fantasy">
           <img src = " http://ecx.images-amazon.com/images/I/51rBfVFsqYL._AA160_.jpg" ><br />
           <input id="WFR" 
                  type="checkbox" name="checkbox[]" 
-                 value="6.91" >
+                 value="Wizards First Rule",6.91, "http://ecx.images-amazon.com/images/I/51rBfVFsqYL._AA160_.jpg" >
           <b>Wizards First Rule</b>
           <br /> Price: $6.91</th>
         <th class="fantasy">
           <img src = " http://ecx.images-amazon.com/images/I/51f1-OdVfuL._AA160_.jpg" ><br /> 
           <input id="GotM" 
                  type="checkbox" name="checkbox[]" 
-                 value="5.44">
+                 value="Gardens of the Moon", 5.44, " http://ecx.images-amazon.com/images/I/51f1-OdVfuL._AA160_.jpg">
           <b>Gardens of the Moon</b>
           <br /> Price: $5.44</th></tr></table>
      
