9028305a5f9c0380b1932d4bb94a649cfd8f8ca7
Steven <and12128@byui.edu>
2018-10-08 22:22:27 -0700

    adding header

--- web/prove/week03/test/header.php
+++ web/prove/week03/test/header.php
@@ -1,6 +1,6 @@
 
 <header id='main' class='w3-container w3-green'>
-    <h1>Brandon's Car Shop</h1>
+    <h1>Fantasy Books</h1>
     <a href='./'>Shop</a>
     <a href='cart.php'>
         Cart (<?php echo count($_SESSION['cartitems']); ?>)
