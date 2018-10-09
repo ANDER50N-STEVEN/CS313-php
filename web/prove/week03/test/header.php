ec106dd82cf444a6b91faafbcd463d0eb988e1e2
Steven <and12128@byui.edu>
2018-10-08 20:53:58 -0700

    testing

---
+++ web/prove/week03/test/header.php
@@ -0,0 +1,9 @@
+
+<header id='main' class='w3-container w3-green'>
+    <h1>Brandon's Car Shop</h1>
+    <a href='./'>Shop</a>
+    <a href='cart.php'>
+        Cart (<?php echo count($_SESSION['cartitems']); ?>)
+    </a>
+    <a href='checkout.php'>Check Out</a>
+</header>
