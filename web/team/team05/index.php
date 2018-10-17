2dd0b6b4fcdb0f62a92a0f42ffcdaca199130ff6
Steven <and12128@byui.edu>
2018-10-16 17:28:13 -0700

    team

--- web/team/team05/index.php
+++ web/team/team05/index.php
@@ -5,11 +5,11 @@
 </head>
 <body>
   <?php
-  $id
-	$book
-	$chapter
-	$verse
-	$content
+  $id;
+	$book;
+	$chapter;
+	$verse;
+	$content;
   echo "<p><span style='font-size:2em; font-weight:bold;'>Scripture Resources</span></p>";
   echo "<p><span style='font-weight:bold'>".$book. " " .  $chapter . ":". $verse ."</span>\"".$content."\"</p>";
   ?>
