c469e177fd9b5adc17c1654ccf0043a349fb05ff
Steven <and12128@byui.edu>
2018-10-16 17:34:00 -0700

    team

--- web/team/team05/index.php
+++ web/team/team05/index.php
@@ -5,37 +5,35 @@
 </head>
 <body>
   <?php
-  try
-{
-  $dbUrl = getenv('DATABASE_URL');
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+      $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
   
-  SELECT * FROM scriptures;
-  
-  $id;
-	$book;
-	$chapter;
-	$verse;
-	$content;
   echo "<p><span style='font-size:2em; font-weight:bold;'>Scripture Resources</span></p>";
-  echo "<p><span style='font-weight:bold'>".$book. " " .  $chapter . ":". $verse ."</span>\"".$content."\"</p>";
+  
+  foreach ($db->query('SELECT id, book, chapter, verse, content FROM scriptures') as $row)
+  {
+		echo "<p><span style='font-weight:bold'>" . $row['id'] . " " .  $row['chapter'] . ":". $row['verse'] ."</span>\"".$row['content']."\"</p>";
+  }  
+  
   ?>
 </body>
 </html>
