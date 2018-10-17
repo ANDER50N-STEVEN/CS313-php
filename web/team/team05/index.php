02edf3b75db70ccd7f9bdbfae6f4b41de0fd2df7
Steven <and12128@byui.edu>
2018-10-16 17:22:59 -0700

    team

---
+++ web/team/team05/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Team 05</title>
+</head>
+<body>
+  <?php
+  $id
+	$book
+	$chapter
+	$verse
+	$content
+  echo "<p><span style='font-size:2em; font-weight:bold;'>Scripture Resources</span></p>";
+  echo "<p><span style='font-weight:bold'>".$book. " " .  $chapter . ":". $verse ."</span>\"".$content."\"</p>";
+  ?>
+</body>
+</html>
