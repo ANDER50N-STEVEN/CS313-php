aa91eafa78b751556ce559223c230158048d2cf9
Steven <and12128@byui.edu>
2018-10-20 15:51:50 -0700

    working on database connection

--- web/prove/week05/signIn.php
+++ web/prove/week05/signIn.php
@@ -1,62 +1,62 @@
-<?php
-session_start();
-?>
+// <?php
+// session_start();
+// ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Sign In<title>  
   
-  // <?php
+  <?php
   
-  	// function test_input($data) {
-      // $data = trim($data);
-      // $data = stripslashes($data);
-      // $data = htmlspecialchars($data);
-      // return $data;
-  // }
+  	function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+  }
   
-    // try
-    // {
-      // $dbUrl = getenv('DATABASE_URL');
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
 
-      // $dbOpts = parse_url($dbUrl);
+      $dbOpts = parse_url($dbUrl);
 
-      // $dbHost = $dbOpts["host"];
-      // $dbPort = $dbOpts["port"];
-      // $dbUser = $dbOpts["user"];
-      // $dbPassword = $dbOpts["pass"];
-      // $dbName = ltrim($dbOpts["path"],'/');
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
 
-      // $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-      // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // }
-    // catch (PDOException $ex)
-    // {
-      // echo 'Error!: ' . $ex->getMessage();
-      // die();
-    // }
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
   
-  // if($_SERVER['REQUEST_METHOD'] == 'POST')
-  // {
-    // if(isset($_POST['pass'])) 
-    // {
-      // $pass = test_input($_POST['pass']);
-      // $query = "SELECT password FROM project1.user";
-	  // $stmt = $db->prepare($query);
-      // $stmt->execute();
-      // $row = $stmt->fetch(PDO::FETCH_ASSOC);
-      // if($row[password] == $pass){
-		  // $_SESSION['name'] = $row[username];
-		  // $_SESSION['user_id'] = $row[user_id];
-		  // $_SESSION['display_name'] = $row[display_name];
-		 // echo '<script>window.location.href = "userPage.php";</script>';
-	  // }
-      // else
-		  // echo "<p>incorrect password, please try again.</p>";
-    // }
-  // }
-  // ?>
+  if($_SERVER['REQUEST_METHOD'] == 'POST')
+  {
+    if(isset($_POST['pass'])) 
+    {
+      $pass = test_input($_POST['pass']);
+      $query = "SELECT password FROM project1.user";
+	  $stmt = $db->prepare($query);
+      $stmt->execute();
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($row[password] == $pass){
+		  $_SESSION['name'] = $row[username];
+		  $_SESSION['user_id'] = $row[user_id];
+		  $_SESSION['display_name'] = $row[display_name];
+		 echo '<script>window.location.href = "userPage.php";</script>';
+	  }
+      else
+		  echo "<p>incorrect password, please try again.</p>";
+    }
+  }
+  ?>
   </head>
 <body>
  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
