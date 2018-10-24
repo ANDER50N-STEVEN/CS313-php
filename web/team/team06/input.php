b09841f4307bdde3de2a48701b835c6de2881f97
Steven <and12128@byui.edu>
2018-10-24 10:13:52 -0700

    trial

--- web/team/team06/input.php
+++ web/team/team06/input.php
@@ -20,8 +20,45 @@
 		$chapter = htmlspecialchars($_POST['chapter']);
 		$verse = htmlspecialchars($_POST['verse']);
 		$content = htmlspecialchars($_POST['content']);
-		$topic = htmlspecialchars($_POST['topic']);
+		$faith = htmlspecialchars($_POST['FAITH']);
+		$sacrifice = htmlspecialchars($_POST['SACRIFICE']);
+		$charity = htmlspecialchars($_POST['CHARITY']);
+		
+		  try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+
+      $dbOpts = parse_url($dbUrl);
+
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+		
+		$stmt = $db->prepare('INSERT INTO scriptures(book, chapter, verse, content) 
+							VALUES (:book, :chapter, :verse, :content);');
+		$stmt->bindValue(':book', $book, PDO::PARAM_STR);
+		$stmt->bindValue(':chapter', $chapter, PDO::PARAM_STR);
+		$stmt->bindValue(':verse', $verse, PDO::PARAM_STR);
+		$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+		$stmt->execute();
+		
+		
+		
 	}
+	
+	
   }
   ?>
 </head>
