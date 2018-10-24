1b7f2c0c915071c6373ff77de26816e964bba134
Steven <and12128@byui.edu>
2018-10-24 09:43:08 -0700

    trial

--- web/team/team06/input.php
+++ web/team/team06/input.php
@@ -34,44 +34,42 @@
   <input type="textarea" id="content" name="content">
   <input type="submit" name="submit" value="Submit">
   
-  <?php
-    try
-    {
-      $dbUrl = getenv('DATABASE_URL');
+  // <?php
+    // try
+    // {
+      // $dbUrl = getenv('DATABASE_URL');
 
-      $dbOpts = parse_url($dbUrl);
+      // $dbOpts = parse_url($dbUrl);
 
-      $dbHost = $dbOpts["host"];
-      $dbPort = $dbOpts["port"];
-      $dbUser = $dbOpts["user"];
-      $dbPassword = $dbOpts["pass"];
-      $dbName = ltrim($dbOpts["path"],'/');
+      // $dbHost = $dbOpts["host"];
+      // $dbPort = $dbOpts["port"];
+      // $dbUser = $dbOpts["user"];
+      // $dbPassword = $dbOpts["pass"];
+      // $dbName = ltrim($dbOpts["path"],'/');
 
-      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      // $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex)
-    {
-      echo 'Error!: ' . $ex->getMessage();
-      die();
-    }
+      // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // }
+    // catch (PDOException $ex)
+    // {
+      // echo 'Error!: ' . $ex->getMessage();
+      // die();
+    // }
   
-      $stmt = $db->prepare('SELECT name FROM topic');
-      $stmt->execute();
-      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      // $stmt = $db->prepare('SELECT name FROM topic');
+      // $stmt->execute();
+      // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
       
-      echo "<p><span style='font-size:2em; font-weight:bold;'>Scripture Resources</span></p>";
-      
-        foreach ($rows as $row)
-        {
+        // foreach ($rows as $row)
+        // {
           
-          echo "<input type='checkbox' name='topic' value=" . "$row['name']" . ">" . $row['name'];
+          // echo "<input type='checkbox' name='topic' value=" . "$row['name']" . ">" . $row['name'];
 
-        }      
+        // }      
   
   
-  ?>
+  // ?>
 </form>
   
 </body>
