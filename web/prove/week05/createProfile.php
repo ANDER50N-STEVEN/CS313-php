cebecdd22e51858d00229fda2e7c8141487a5b3f
Steven <and12128@byui.edu>
2018-10-26 13:59:33 -0700

    start create profile

---
+++ web/prove/week05/createProfile.php
@@ -0,0 +1,49 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" type="text/css" href="../stylesheet.css" />
+			<title>User Select</title>
+			<?php
+				require('dbConnect.php');
+				
+			?>
+	</head>
+		<body>
+			<?php
+				require('../header.php');
+				if($_SERVER['REQUEST_METHOD'] == 'POST')
+				{
+					$name = htmlspecialchars($_POST['name']);
+					$userName = htmlspecialchars($_POST['userName']);
+					$pass = htmlspecialchars($_POST['pass']);
+					
+					$stmt = $db->prepare('INSERT INTO project1.user(username, password, display_name) 
+							VALUES (:name, :pass, :userName)');
+					$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+					$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
+					$stmt->bindValue(':userName', $userName, PDO::PARAM_STR);
+					$stmt->execute();
+					
+				}
+			?>
+			<form method="post" action="New.php">
+			
+			<label for="message">What is your full name</label></br>
+		<input type="text" id="name" name="name"></br>
+			
+		<label for="username"> Please Enter A Username</label></br>
+		<input type="text" id="userName" name="userName"></br>
+
+		<label for="message">Please Enter A Password</label></br>
+		<input type="text" id="pass" name="pass"></br>
+		<input type="submit" name="submit" value="Submit">
+		</form></br> 
+			<?php
+			require('../../footer.php');
+		?>
+	</body>
+</html>
