30dd3f9f9f254e83da38d4b3f705b61ff3725314
Steven <and12128@byui.edu>
2018-10-26 12:35:51 -0700

    modify session

--- web/prove/week05/New.php
+++ web/prove/week05/New.php
@@ -16,8 +16,15 @@ session_start();
  <?php
     require('../header.php');
     ?>
-  
-  
+	<form method="post" action="<?php echo htmlspecialchars("userPage.php");?>">
+	<label for="username"> Please Enter Your Username</label>
+	<input type="text" id="userName" name="userName">
+	<input type="submit" name="submit" value="Submit">
+	<label for="message">Please Enter Your Password</label>
+	<?php echo "SELECT password, username, id, display_name FROM project1.user WHERE id = ". $_SESSION['id']; ?>
+	<input type="text" id="pass" name="pass">
+	<input type="submit" name="submit" value="Submit">
+</form>
    <?php
     require('../../footer.php');
     ?>
