86d588495ad814c30950e89335760ac7eea9ad22
Steven <and12128@byui.edu>
2018-10-26 16:29:35 -0700

    start create review page

--- web/prove/week05/createBookReview.php
+++ web/prove/week05/createBookReview.php
@@ -16,19 +16,6 @@
 			<?php
 				require('../header.php');
 				echo $create . "before";
-				if($_SERVER['REQUEST_METHOD'] == 'GET')
-				{
-					$title = htmlspecialchars($_GET['title']);
-			
-					$stmt = $db->prepare('SELECT title
-											From project1.library');
-					$stmt->execute();
-					$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-					foreach($rows as $row){
-						if($row['title'] == $title)
-							$create = true;
-					}				
-				}
 				
 				if($_SERVER['REQUEST_METHOD'] == 'POST'){
 					$title = htmlspecialchars($_POST['title']);
@@ -37,6 +24,14 @@
 					$rating = $_POST['rating'];
 					$review = htmlspecialchars($_POST['review']);
 					
+					$stmt = $db->prepare('SELECT title
+											From project1.library');
+					$stmt->execute();
+					$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+					foreach($rows as $row){
+						if($row['title'] == $title)
+							$create = true;
+					}				
 					
 				}
 				echo $create . "after";
