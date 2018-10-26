df080eb0f17800d3f3775c1645f82ace1b888154
Steven <and12128@byui.edu>
2018-10-26 14:57:28 -0700

    start create profile

--- web/prove/week05/New.php
+++ web/prove/week05/New.php
@@ -5,7 +5,7 @@
 <html lang="en">
 	<head>
 		<link rel="stylesheet" type="text/css" href="../stylesheet.css" />
-			<title>User Select</title>
+			<title>SIGN IN</title>
 			<?php
 				require('dbConnect.php');
 			?>
