18dad1827e8bd9050943532d671679714d2fce3a
Steven <and12128@byui.edu>
2018-10-27 12:44:53 -0700

    trial

---
+++ web/team.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC >
+<html lang = "english">
+  
+  <head><title> Team Assignements </title>
+    <meta charset = "utf-8">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
+  <body>   
+<?php
+ include('header.php')
+?>
+<h1 class ="assignList"> List of Assignements </h1>
+
+<ul class ="assignList">
+        <li><a href = "team/team02/Ponder02"> Week 2 - Home Page </a></li>
+        <li><a href = "team/team03/teamActivity"> Week 3 - Shopping Cart </a></li>
+
+        <li><a href = "team/team05/index.php"> Week 5 </a></li>
+        <li><a href = "team/team06/input.php"> Week 6 </a></li>
+        <li><a href = "team/team07/browse.php"> Week 7 </a></li>
+        <li><a href = "team/team08/browse.php"> Week 8 </a></li>
+        <li><a href = "team/team09/browse.php"> Week 9 </a></li>
+        <li><a href = "team/team10/browse.php"> Week 10 </a></li>
+        <li><a href = "team/team11/browse.php"> Week 11 </a></li>
+        <li><a href = "team/team12/browse.php"> Week 12 </a></li>
+        <li><a href = "team/team13/browse.php"> Week 13 </a></li>
+		</ul>
+<?php
+include('footer.php')
+?>
