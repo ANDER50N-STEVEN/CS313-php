13265330f942f644109c4520e11de634a72fa5c4
Steven <and12128@byui.edu>
2018-10-27 12:46:27 -0700

    update homepage

--- web/team.php
+++ web/team.php
@@ -15,8 +15,8 @@
         <li><a href = "team/team02/Ponder02"> Week 2 - Home Page </a></li>
         <li><a href = "team/team03/teamActivity"> Week 3 - Shopping Cart </a></li>
 
-        <li><a href = "team/team05/index.php"> Week 5 </a></li>
-        <li><a href = "team/team06/input.php"> Week 6 </a></li>
+        <li><a href = "team/team05/index.php"> Week 5 - Search Database </a></li>
+        <li><a href = "team/team06/input.php"> Week 6 - Input Database </a></li>
         <li><a href = "team/team07/browse.php"> Week 7 </a></li>
         <li><a href = "team/team08/browse.php"> Week 8 </a></li>
         <li><a href = "team/team09/browse.php"> Week 9 </a></li>
