3c1b76c06f8d110c4b46f1cf2ff197070d0081cb
Steven <and12128@byui.edu>
2018-10-26 17:05:32 -0700

    start create review page

--- db/project1_updated.sql
+++ db/project1_updated.sql
@@ -1,17 +1,6 @@
-﻿#	Firstly I would like it noted that I have no experience with this and 
-#	would appreciate any and all criticism/recommendations that you or anyone
-#	could give me.  Thank You
-#
-#	I didn't know how to create a new database, or if I should, so I hoped 
-#	creating a  schema would be what I needed to do.
 
 CREATE SCHEMA project1;
 
-#	I don't think there is any other information I need contained in this
-#	table.  I do want a list of all the books they have reviewed, as well
-#	as hopefully a list of all the books recommended to them, but I think
-#	I can pull that information from other sources, namely the review table
-#	by user_id for the books they've reviewed.
 
 CREATE TABLE project1.user
 (
@@ -21,9 +10,6 @@ CREATE TABLE project1.user
 	display_name VARCHAR(100) NOT NULL
 );
 
-#	This seemed like a waste because I only use author once so I ended up 
-#	changing the library section.  should I keep this for instances where I
-# 	might want access to it or get rid of it?
 
 CREATE TABLE project1.author
 (
@@ -31,7 +17,6 @@ CREATE TABLE project1.author
 	author_name VARCHAR(100) NOT NULL UNIQUE
 );
 
-#	Again trying to eliminate redundency, but I don't know if its necessary.
 
 CREATE TABLE project1.genre
 (
@@ -39,10 +24,6 @@ CREATE TABLE project1.genre
 	genre VARCHAR(100) NOT NULL UNIQUE 
 );
 
-#	This contains a list of all the books availiable.  My original thought was
-#	to populate it with hundreds of books, but that took too long.  Now my 
-#	idea is that as an individual reviews a book it will search this or create
-#	a new element in the table...hopefully I can do that.
 
 CREATE TABLE project1.library
 (
@@ -53,8 +34,6 @@ CREATE TABLE project1.library
 	summary TEXT
 );
 
-#	Is there a way to limit the imput of rating?  Or is that just something
-#	I do on another level?
 
 CREATE TABLE project1.rating
 (
@@ -65,8 +44,6 @@ CREATE TABLE project1.rating
 	review TEXT
 );
 
-#	Inserting genres first was necessary to create the library.  There need
-#	to be more, or just let people add them like the library.
 
 INSERT INTO project1.genre (id, genre) VALUES
 (1, 'Fantasy'),
@@ -77,7 +54,7 @@ INSERT INTO project1.genre (id, genre) VALUES
 
 
 
-INSERT INTO project1.author (author) VALUES
+INSERT INTO project1.author (author_name) VALUES
 ('George R.R. Martin')  ,
 ('J.R.R. Tolkien')  ,
 ('Patrick Rothfuss'),
@@ -95,7 +72,7 @@ INSERT INTO project1.author (author) VALUES
 ('William Goldman')  ,
 ('David Eddings' ),
 ('Marion Zimmer Bradley'),  
-('Raymond E. Feist') 
+('Raymond E. Feist'); 
 
 INSERT INTO project1.library (id, title, author_id, genre_id) VALUES
 (1, 'A Game of Thrones', 1, 1)  ,
@@ -123,8 +100,6 @@ INSERT INTO project1.user (username, password, display_name) VALUES
 ('Steven Anderson', 'password', 'MisterANDER50N'),
 ('Katherine Anderson', 'avatar', 'Kate Lynn');
 
-#	I realized book_id and library might be confusing.  might need to change
-#	that.  
 
 INSERT INTO project1.rating (id, user_id, book_id, rating, review) VALUES
 (1, 1, 5, 5, 'This is one of my favorite books, not because the book itself is that unique, but because it sets the stage and creates characters you learn to love'),
