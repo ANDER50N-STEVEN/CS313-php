04ba0f4e688f17a0141035db1fa8e67a0f1aad74
Steven <and12128@byui.edu>
2018-10-31 13:23:26 -0700

    team 07 sign in

--- web/team/team07/sign-up.php
+++ web/team/team07/sign-up.php
@@ -16,18 +16,24 @@
 				{
 					$userName = htmlspecialchars($_POST['userName']);
 					$pass = htmlspecialchars($_POST['pass']);
-					$passwordHash = password_hash($pass, PASSWORD_DEFAULT);
+					$test = htmlspecialchars($_POST['test']);
+					if ($pass == $test) {
+						$passwordHash = password_hash($pass, PASSWORD_DEFAULT);
 					
-					$stmt = $db->prepare('INSERT INTO simple(username, password) 
-							VALUES (:userName, :pass)');
-					$stmt->bindValue(':userName', $userName, PDO::PARAM_STR);
-					$stmt->bindValue(':pass', $passwordHash, PDO::PARAM_STR);
-				
-					$stmt->execute();
-					$new_Page ="sign-in.php";
-					header("Location: $new_Page");
-					die();
+						$stmt = $db->prepare('INSERT INTO simple(username, password) 
+								VALUES (:userName, :pass)');
+						$stmt->bindValue(':userName', $userName, PDO::PARAM_STR);
+						$stmt->bindValue(':pass', $passwordHash, PDO::PARAM_STR);
 					
+						$stmt->execute();
+						$new_Page ="sign-in.php";
+						header("Location: $new_Page");
+						die();
+					
+					} else  {
+						echo "You entered two different passwords";
+					}
+			
 				}
 			?>
 		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
@@ -37,6 +43,10 @@
 
 		<label for="pass">Please Enter A Password</label></br>
 		<input type="password" id="pass" name="pass"></br>
+		<label for="test">Please Re-enter your Password</label></br>
+		<input type="passwor" id="test" name="test">
+
+		</br>
 		<input type="submit" name="submit" value="Submit">
 		</form></br> 
 	</body>
