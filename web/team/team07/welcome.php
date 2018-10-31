5d794fef71d9cf8f42f239194fef1d63c8321f75
Steven <and12128@byui.edu>
2018-10-31 12:42:28 -0700

    team 07 sign in

--- web/team/team07/welcome.php
+++ web/team/team07/welcome.php
@@ -4,24 +4,16 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link rel="stylesheet" type="text/css" href="../stylesheet.css" />
-			<title>SIGN IN</title>
+		<title>Welcome</title>
 			<?php
 				require('dbConnect.php');
 			?>
 	</head>
 		<body>
 			<?php
-				require('../header.php');
-
-				if($_SERVER['REQUEST_METHOD'] == 'POST')
-				{
-					if(isset($_POST['pass']))
-					{
-						$userName = htmlspecialchars($_POST['userName']);
-						$pass = htmlspecialchars($_POST['pass']);
-						$query = "SELECT password, username, id, display_name 
-						FROM project1.user";
+				
+						$query = "SELECT password, username, id
+						FROM simple";
 						
 						$stmt = $db->prepare($query);
 						$stmt->execute();
@@ -29,36 +21,17 @@
 
 						foreach ($rows as $row)
 						{
-							if($row['password'] == $pass && $row['display_name'] == $userName){
-							$_SESSION['name'] = $row['username'];
-							$_SESSION['user_id'] = $row['id'];
-							$_SESSION['display_name'] = $row['display_name'];
-							$new_Page ="userPage.php";
+							if($row['display_name'] == $_SESSION['userName']){
+							echo "Welcome " . $_SESSION['userName'];
+							
+							}
+							else{
+								$new_Page ="sign-in.php";
 							header("Location: $new_Page");
 							die();
 							}
 						}
-						echo "<p>incorrect password, please try again.</p>";	
-					}
-				}
 			?>
-		
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-		<label for="username"> Please Enter Your Username</label></br>
-		<input type="text" id="userName" name="userName"></br>
-
-		<label for="message">Please Enter Your Password</label></br>
-		<input type="text" id="pass" name="pass"></br>
-		<input type="submit" name="submit" value="Submit">
-		</form></br> 
-		<form action="createProfile.php">
-		<input type="submit" value="Create Profile" />
-		</form>
-		
-		
-
-		<?php
-			require('../../footer.php');
-		?>
+	
 	</body>
 </html>
