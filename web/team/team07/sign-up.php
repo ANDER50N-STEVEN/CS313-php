b84f482c69eac1c6bba33b13844452e51aa127b3
Steven <and12128@byui.edu>
2018-10-31 12:01:54 -0700

    team 07 sign in

---
+++ web/team/team07/sign-up.php
@@ -0,0 +1,42 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Sign Up</title>
+			<?php
+				require('dbConnect.php');
+			?>
+	</head>
+		<body>
+			<?php
+				if($_SERVER['REQUEST_METHOD'] == 'POST')
+				{
+					$userName = htmlspecialchars($_POST['userName']);
+					$pass = htmlspecialchars($_POST['pass']);
+					
+					$stmt = $db->prepare('INSERT INTO simple(username, password) 
+							VALUES (:userName, :pass)');
+					
+					$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
+					$stmt->bindValue(':userName', $userName, PDO::PARAM_STR);
+					$stmt->execute();
+					$new_Page ="sign-in.php";
+					header("Location: $new_Page");
+					die();
+					
+				}
+			?>
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			
+		<label for="username"> Please Enter A Username</label></br>
+		<input type="text" id="userName" name="userName"></br>
+
+		<label for="pass">Please Enter A Password</label></br>
+		<input type="text" id="pass" name="pass"></br>
+		<input type="submit" name="submit" value="Submit">
+		</form></br> 
+	</body>
+</html>
