4d21337040f3a048d1bf8f108909d138fc5dd00f
Steven <and12128@byui.edu>
2018-10-31 13:43:59 -0700

    team 07 sign in

--- web/team/team07/sign-up.php
+++ web/team/team07/sign-up.php
@@ -18,7 +18,7 @@
 					$pass = htmlspecialchars($_POST['pass']);
 					$test = htmlspecialchars($_POST['test']);
 					if ($pass == $test) {
-						if(preg_match(=(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,15}){
+						if(preg_match((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,15})){
 							$passwordHash = password_hash($pass, PASSWORD_DEFAULT);
 						
 							$stmt = $db->prepare('INSERT INTO simple(username, password) 
